dbf2ffa24ca61fb867585a6e8af7611b339c6857
YSLErnst <1071884800@qq.com>
2017-10-27 17:33:32 +0800

    Revert "[ADD]增加23种设计模式"

--- src/main/java/com/hand/ysl/demo/design/responsibilityDesign/Interface/Handler.java
+++
@@ -1,8 +0,0 @@
-package com.hand.ysl.demo.design.responsibilityDesign.Interface;
-
-/**
- * Created by Ernst on 2017/10/9.
- */
-public interface Handler {
-    public void operator();
-}
--- src/main/java/com/hand/ysl/demo/design/strategyDesign/Interface/ICaculator.java
+++
@@ -1,8 +0,0 @@
-package com.hand.ysl.demo.design.strategyDesign.Interface;
-
-/**
- * Created by Ernst on 2017/9/11.
- */
-public interface ICaculator {
-    public int caculate(String exp);
-}
